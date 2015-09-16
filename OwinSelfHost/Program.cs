6c769f0f0e90f46cb36c3d1a96d605ec0ca5b6f4
test <test@TEST.COM>
2015-09-16 20:17:43 +0300

    Initial commit

---
+++ OwinSelfHost/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace OwinSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:9999"))
+            {
+                Console.WriteLine("Press ENTER to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
